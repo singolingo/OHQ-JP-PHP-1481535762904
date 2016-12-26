935a85284b6fe43308a355fac04f534ceacd3694
030144714 <030144714@SPR8701.ad.jp.omron.net>
2016-12-26 17:02:52 +0900

    Change-Id: I05ed84d6ecdcc9ae66b521953b6271a7de4cd08f

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	<title>PHP Starter Application</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
+
+  <!-- Nifty Cloud SDKの読み込み -->
+  <script type="text/javascript" src="js/ncmb.min.js" charset="utf-8"></script>
+
 </head>
 <body>
 	<table>
@@ -12,7 +16,7 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+				<h1 id = "message"><?php echo "OMRON connect Demo"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>

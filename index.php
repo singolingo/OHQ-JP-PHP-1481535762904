2dd7d786e6f025ca6ccfdb39852f30736d7817ee
030144714 <030144714@SPR8701.ad.jp.omron.net>
2016-12-27 17:45:39 +0900

    Change-Id: I0000000000000000000000000000000000000002

--- index.php
+++ index.php
@@ -5,21 +5,66 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 
-  <!-- Nifty Cloud SDKの読み込み -->
-  <script type="text/javascript" src="js/ncmb.min.js" charset="utf-8"></script>
+  <script type="text/javascript" src="js/ncmb.min.js" charset="utf-8">
+
+  <!-- Nifty Cloud  -->
+    //【環境センサー】 ニフティクラウドに接続
+    var ncmb = new NCMB(e34bf31c6652e31c561f3f0253bd13a46ace822c266a490e69d13c51109f0106,1e58451ab1c41d53824514f79552c0a718716af91e898f8418494bf22132b416);
+    //【OHQ】 ニフティクラウドに接続
+    //var ncmb = new NCMB(960534844e162f267e64764aa91ed32cbdf73a4582451afe5be15a6bec788bf6,0072db3fd89db23a26cef2ef707c9f4f42035f8a8a69cc22dc7c881c49225934);
+
+    // クラスのTestClassを作成
+    var OC_KankyoSensor = ncmb.DataStore("OC_KankyoSensor");
+
+    // データストアへの登録
+    var OC_KankyoSensor = new OC_KankyoSensor();
+    OC_KankyoSensor.set("message", "This is A message.");
+    OC_KankyoSensor.save()
+         .then(function(){                 // 保存に成功した場合の処理
+
+
+
+          })
+         .catch(function(err){            // 保存に失敗した場合の処理
+
+
+
+          });
+
+
+    <!-- OGSC Cloud  -->
+     // ヘッダーを指定
+	header( "Content-Type: application/json; charset=utf-8" ) ;
+	$url = "http://OGSC.com"; // OGSC Cloud API サーバーのURL　＆　アクセスキー
+	$json = file_get_contents($url);
+	//mb_language("Japanese");  php.ini の設定が難しい場合は、これを宣言。
+	$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+	$arr = json_decode($json,true);
+
+	if ($arr === NULL) {      //〜データがない時の処理〜
+	    return;
+	}else{	                  //〜存在しているときの処理〜
+     $BPmax= 0;
+     $BPmin= 0;
+     $Shinpaku= 0;
+
+	}
+
+  </script>
 
 </head>
 <body>
 	<table>
 		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
 			<td>
 				<h1 id = "message"><?php echo "OMRON connect Demo"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">OMRON connect Demo Application</span>.
 			</td>
 		</tr>
+
+
+
+
 	</table>
 </body>
 </html>

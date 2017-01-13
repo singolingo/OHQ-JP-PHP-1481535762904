6c774eb2187928000148bb1c5c07bc2bef187333
030144714 <030144714@SPR8701.ad.jp.omron.net>
2017-01-13 10:51:57 +0900

    1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>OGSC＆環境センサのデモ</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta charset="utf-8">
-    <meta name="description" content="これはデモです。">
+    <meta name="description" content="これはデモです。 ">
 
 <!-- 	<link rel="stylesheet" href="style.css" /> -->
 
